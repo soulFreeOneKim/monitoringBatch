c3e86f03a293cc035618246b98b2475140c36c62
soulFreeOneKim <soulfree89@naver.com>
2023-08-29 18:27:30 +0900

    commit

--- detector.py
+++ detector.py
@@ -3,6 +3,8 @@ import pandas as pd
 import os
 import logging
 import datetime
+# from datetime import datetime, date
+
 
 FILE_ABSOLUTE_PATH  = os.path.abspath(__file__)
 ROOT_DIR            = os.path.dirname(FILE_ABSOLUTE_PATH)
@@ -43,6 +45,12 @@ class Detector:
         mode = 'w' if not is_file_created_today(f'{RESULT_DIR}/result.txt') else 'a'
 
         with open(f'{RESULT_DIR}/result.txt', mode) as file:
+            # 현재 시간 가져오기
+            current_time = datetime.datetime.now()
+
+            # 연월시분초 포맷으로 출력
+            formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
+            file.write(f"**************************************** {formatted_time} **************************************** \n")
             file.write(f"[Checker Start] -------------------------------------------------- \n")
             file.write(f"HOST_NAME : {self.LOGINFO.LOG_HOST_NM}, APP_DV_CD : {self.LOGINFO.APP_DV_CD}, CENTER_DV_CD :{self.LOGINFO.CENTER_DV_CD} \n")
 
@@ -50,21 +58,27 @@ class Detector:
             # 로그별 체크 로직
             # ----------------------------------------------------------------------------------------------- #
             if self.LOGINFO.APP_DV_CD == "VGW" and self.LOGINFO.CENTER_DV_CD == "CALL-CENTER":
-                pass
+
+
+
+                file.write(f"[Checker End] -------------------------------------------------- \n")
             elif self.LOGINFO.APP_DV_CD == "VGW" and self.LOGINFO.CENTER_DV_CD == "DIRECT-CENTER": 
-                pass
+
+
+                
+                file.write(f"[Checker End] -------------------------------------------------- \n")
 
 
             elif self.LOGINFO.APP_DV_CD == "VSA" and self.LOGINFO.CENTER_DV_CD == "COMMON":
                 df = DF
-                # --- db connection check
+                # --- DB CONNECTION CHECK
                 df_dbconnection = df[df['DETAIL_TXT'].str.contains("HikariPool")]
                 con_failed_cnt = len(df_dbconnection[df_dbconnection['DETAIL_TXT'].str.contains("failed")])
                 file.write(f"DB CONNECTION FAIL COUNT : {con_failed_cnt}\n")
                 if con_failed_cnt != 0:
                     file.write(f"[ERROR] ---------- DB CONNECTION FAIL OCCURRED ... \n")                   
 
-                # --- EAI INTERFACE FAILED
+                # --- EAI INTERFACE CHECK
                 df = df[~df['DETAIL_TXT'].str.contains("HikariPool")]
                 df = df[df['DETAIL_TXT'].str.contains("\[CC")]
                 df = df[df['DETAIL_TXT'].str.contains("EAI REQUEST")]
@@ -88,14 +102,14 @@ class Detector:
 
             elif self.LOGINFO.APP_DV_CD == "VDT" and self.LOGINFO.CENTER_DV_CD == "COMMON": 
                 df = DF
-                # --- db connection check
+                # --- DB CONNECTION CHECK
                 df_dbconnection = df[df['DETAIL_TXT'].str.contains("HikariPool")]
                 con_failed_cnt = len(df_dbconnection[df_dbconnection['DETAIL_TXT'].str.contains("failed")])
                 file.write(f"DB CONNECTION FAIL COUNT : {con_failed_cnt}\n")
                 if con_failed_cnt != 0:
                     file.write(f"[ERROR] ---------- DB CONNECTION FAIL OCCURRED ... \n") 
 
-                # --- EAI INTERFACE FAILED
+                # --- EAI INTERFACE CHECK
                 df = df[~df['DETAIL_TXT'].str.contains("HikariPool")]
                 df = df[df['DETAIL_TXT'].str.contains("\[CD")]
                 df = df[df['DETAIL_TXT'].str.contains("EAI REQUEST")]
@@ -117,11 +131,40 @@ class Detector:
                     file.write(f"[Checker End] -------------------------------------------------- \n")
 
             elif self.LOGINFO.APP_DV_CD == "VRS" and self.LOGINFO.CENTER_DV_CD == "COMMON": 
-                logging.info(f"55555")
+                df = DF
+                # --- DB CONNECTION CHECK
+                df_dbconnection = df[df['DETAIL_TXT'].str.contains("HikariPool")]
+                con_failed_cnt = len(df_dbconnection[df_dbconnection['DETAIL_TXT'].str.contains("failed")])
+                file.write(f"DB CONNECTION FAIL COUNT : {con_failed_cnt}\n")
+                if con_failed_cnt != 0:
+                    file.write(f"[ERROR] ---------- DB CONNECTION FAIL OCCURRED ... \n") 
+                
+                # --- EAI INTERFACE CHECK 
+                df = df[~df['DETAIL_TXT'].str.contains("HikariPool")]
+                df_reqResistDirect_req = df[ df['DETAIL_TXT'].str.contains("reqResistDirect") & df['DETAIL_TXT'].str.contains("paramBytes.length") ]
+                df_reqResistDirect_req['DV_CD'] = 'REQ:reqResistDirect'
 
+                df_responseHistory_req = df[ df['DETAIL_TXT'].str.contains("responseHistory") & df['DETAIL_TXT'].str.contains("paramBytes.length") ]
+                df_responseHistory_req['DV_CD'] = 'REQ:responseHistory'
 
+                df_eai_res = df[ df['DETAIL_TXT'].str.contains("makeEaiCommonHeader") & df['DETAIL_TXT'].str.contains("PRCS_RSLT_DVCD") ]
+                df_eai_res['DV_CD'] = 'RES:' + df_eai_res['DETAIL_TXT'].str.extract(r'\[(\d+)\]$')
 
-        #logging.info(f"DataFrame : {self.DF}")
+                df_eai_req_res = pd.concat([df_reqResistDirect_req, df_responseHistory_req,df_eai_res], axis=0).sort_values(by='LOG_LINE')
+                
+                eai_stat_err_cnt = {'reqResistDirect' : 0, 'responseHistory' : 0}
+                for idx , val in enumerate(df_eai_req_res['DV_CD']):
+                    if val.split(":")[-1] == 'reqResistDirect' or val.split(":")[-1] == 'responseHistory':
+                        continue
+                    else:
+                        if val.split(":")[-1] != "0":
+                            eai_stat_err_cnt[df_eai_req_res['DV_CD'].values[idx-1].split(":")[-1]] = \
+                                eai_stat_err_cnt[df_eai_req_res['DV_CD'].values[idx-1].split(":")[-1]] + 1
+                
+                if eai_stat_err_cnt['reqResistDirect'] > 0 : file.write(f"[ERROR] ---------- [reqResistDirect] RESPONSE FAILED OCCURRED ... \n")
+                if eai_stat_err_cnt['responseHistory'] > 0 : file.write(f"[ERROR] ---------- [responseHistory] RESPONSE FAILED OCCURRED ... \n")
+                
+                file.write(f"[Checker End] -------------------------------------------------- \n")
 
         logging.info(f"[detector.py] jobProcessingChecker ended")
 
